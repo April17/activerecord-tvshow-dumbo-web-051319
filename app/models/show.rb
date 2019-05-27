1f93cf9db67f156fcbc6ed04791e563cd89fea1b
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-27 05:36:45 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end
   def self.most_popular_show
-    self.where("rating = #{highest_rating}")
+    all.find {|show| show.rating = highest_rating}
   end
   def self.lowest_rating
     self.minimum(:rating)
