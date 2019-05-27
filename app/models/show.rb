3981ca373d2d973fe9fc41d2ae91c321e84cd414
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-27 05:29:45 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+  def self.most_popular_show
+    self.where("rating = ?", highest_rating)
+  end
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  def self.least_popular_show
+    self.where("rating = ?", lowest_rating)
+  end
 end
